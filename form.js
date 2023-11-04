8f53bcaa687acef77c1ea6132bfb3287a7b5f772
aaron8a93 <142675696+aaron8a93@users.noreply.github.com>
2023-11-04 15:01:35 -0700

    preview blank link

--- form.js
+++ form.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("myForm");
-    const result = document.getElementById("result");
+  const form = document.getElementById("myForm");
+  const result = document.getElementById("result");
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent the default form submission behavior.
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevents default form submission
 
-        // Collect form data into an object.
-        const formData = new FormData(form);
-        const formDataObject = {};
+    // Collect form data into object
+    const formData = new FormData(form);
+    const formDataObject = {};
 
-        formData.forEach((value, key) => {
-            formDataObject[key] = value;
-        });
-
-        // Convert the form data object to JSON.
-        const formDataJSON = JSON.stringify(formDataObject, null, 2);
-
-        // Display the JSON data.
-        result.textContent = formDataJSON;
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
     });
+
+    // Convert the form data object to JSON
+    const formDataJSON = JSON.stringify(formDataObject, null, 2);
+
+    // Display the JSON data
+    result.textContent = formDataJSON;
+  });
 });
